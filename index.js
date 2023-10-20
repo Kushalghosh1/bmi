17ff01e5f0f6dc38d057659e09f80e0dddedcf3c
Kushalghosh1 <cse.kushal1@gmail.com>
2023-10-20 19:00:50 +0600

    finished the bmi project

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// const form = document.querySelector('form')
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    const results = document.querySelector('#results');
+
+    if (height === '' || height < 0 || isNaN(height)){
+        results.textContent = 'Please fill valid weight';
+    }
+
+    else if (weight === '' || weight < 0 || isNaN(weight)){
+        results.textContent = 'Please fill valid weight';
+    }
+
+    else{
+        const bmi = (weight / ((height * height) / 1000)).toFixed(2);
+        if (bmi < 18.6){
+            results.textContent = `Your BMI is ${bmi}, You are Underweight
+            `;
+        }
+
+        else if (bmi < 25){
+            results.textContent = `Your BMI is ${bmi}, You are Normal
+            `;
+        }
+        else if (bmi < 30){
+            results.textContent = `Your BMI is ${bmi}, You are Overweight
+            `;
+        }
+        else {
+            results.textContent = `Your BMI is ${bmi}, You are Obese
+            `;
+        }
+    }
+})
